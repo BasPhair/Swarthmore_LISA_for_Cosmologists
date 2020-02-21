36bd22e89e8078dd0fcecc5b27d964a7085e1657
BasPhair <32944263+BasPhair@users.noreply.github.com>
2020-02-21 14:06:01 -0500

    Basically all the Mathemtica!!!

--- Sensitivity_to_GW150914.py
+++ Sensitivity_to_GW150914.py
@@ -4,6 +4,8 @@ Sensitivity to GW150914
 from scipy import integrate
 from LISA_Object import LISA
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 lisa = LISA('LISA_Data.csv')
 
 
@@ -22,7 +24,7 @@ Z = .09
 Mr = (((M1*M2)**3)/(M1+M2))**.2
 
 #Distance
-integrand = lambda x: 2997.9/(.7/(math.sqrt(((.3*(1+x))**3)+.7)))
+integrand = lambda x: 2997.9/.7/math.sqrt((.3*(1+x)**3)+.7)
 Dcom = Mpc*integrate.quad(integrand,0,Z)[0]
 DL = (1+Z)*Dcom
 
@@ -35,10 +37,21 @@ Tc = lambda frad: ((5*(c**5))/(256*(fr**(8/3))*((GN*Mr)**(5/3))*(math.pi()**(8/3
 
 #... in observer frame frame (eq 98)
 #check that it radiates for 4 years
-print(((5*(c**5))/(256*(fr**(8/3))*((GN*Mr)**(5/3))*(math.pi**(8/3))))/((lisa.Year)*(1+Z)))
+print(((5*(c**5))/(256*(fr**(8/3))*((GN*Mr)**(5/3))*(math.pi**(8/3))))/(lisa.Year)*(1+Z))
 
 #characteristic strain (eq 99)
-hc = (math.sqrt(2/3)*(((GN*Mr*(1+Z))/(c**3))**(5/6)))/((((math.pi**(2/3))*DL)/c)*(fi**(-1/6)))
-hcf = lambda f: hc(f/fi)**(-1/6)
-#integrand = lambda 
-#SNR = math.sqrt(integrate.quad(
+hc = ((math.sqrt(2/3)*(((GN*Mr*(1+Z))/(c**3))**(5/6)))*(fi**(-1/6)))/((math.pi**(2/3))*DL/c)
+hcf = lambda f: hc*((f/fi)**(-1/6))
+integrand1 = lambda f: ((hcf(f)/(2*f))**2)/lisa.SumHApprox(f)
+integrand2 = lambda f: ((hcf(f)/(2*f))**2)/lisa.SumHNum(f)
+SNR1 = math.sqrt(integrate.quad(integrand1, fi, 1)[0])
+print(SNR1)
+SNR2 = math.sqrt(integrate.quad(integrand2, fi, 1)[0])
+print(SNR2)
+
+x = np.logspace(-5,0,1000)
+x2 = np.logspace(math.log(fi,10),math.log(.05,10),200)
+plt.loglog(x,[math.sqrt(4*i*lisa.SumHApprox(i)) for i in x], x2,[hcf(i) for i in x2],'--r')
+plt.title('Sensitivity of LISA to GW150914')
+plt.show()
+
